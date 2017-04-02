d4b6f60881044ddb1e495f6c95c365343b1214a3
Joe DeLorenzo <jdelorenzo@intuitivecompany.com>
2017-04-02 16:47:18 -0400

    statred spitting out our fun tweets

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var async = require('async');
 var request = require('request');
+var adjectives = require('adjectives');
 var _ = require('lodash');
 var schedule = require('node-schedule');
 
@@ -7,28 +8,22 @@ var github = require('./lib/github');
 var twitter = require('./lib/twitter');
 var local = require('./local');
 
-github.getRepos({ q: 'sadness' }, function (error, res, body) {
-  if (error) {
-    console.log(error);
-  }
-  var results = JSON.parse(body); 
 
-  return github.getBranch(results.items, function(goods) {
-    // console.log(goods);
-    twitter.tweet(function(){
-    	console.log('we done');
-    })
+// schedule syntax
+
+var j = schedule.scheduleJob({ rule: '*/10 * * * * *' }, function(){
+  var myAdj = _.shuffle(adjectives)[0];
+  github.getRepos({ topic: myAdj }, function (error, res, body) {
+    if (error) {
+      console.log(error);
+    }
+    var results = JSON.parse(body); 
+
+    return github.getBranch(results.items, function(goods) {
+      twitter.tweet(_.shuffle(goods)[0], function(){
+        console.log("myAdj: "+ myAdj);
+        console.log('we done');
+      })
+    });
   });
 });
-
-
-
-// schedule syntax for later
-
-// var app = function() {
-//   console.log('hello');
-// }
-
-// var j = schedule.scheduleJob({ rule: '*/1 * * * * *' }, function(){
-//   app();
-// });
