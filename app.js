9504c3298f48cfb9ac001b301468f7bfdd4e4136
Stuart Romanek <stuart@punkave.com>
2017-04-20 08:11:49 -0400

    switched to real environment vars for credentialing, set to run every morning at 7;30am

--- app.js
+++ app.js
@@ -1,33 +1,45 @@
+require('dotenv').config()
 var async = require('async');
 var request = require('request');
 var adjectives = require('adjectives');
 var _ = require('lodash');
 var schedule = require('node-schedule');
 
-
 var github = require('./lib/github');
 var twitter = require('./lib/twitter');
-var local = require('./local');
 
-var j = schedule.scheduleJob({ rule: '*/10 * * * * *' }, function(){
-  var myAdj = _.shuffle(adjectives)[0];
-  github.getRepos({ q: myAdj }, function (error, res, body) {
-    if (error) {
-      console.log(error);
-    }
-    var results = JSON.parse(body); 
+console.log('STARTING UP');
 
-    return github.findGhPages(results.items, function(goods) {
-      if (goods.length) {
-        var theRepo = _.shuffle(goods)[0];
-        return github.getGhImg(theRepo, function(repo) {
-          twitter.tweet(repo, function(){
-            console.log('finished');
-          })
-        });
-      } else {
-        console.log('no result');
+var j = schedule.scheduleJob({hour: 7, minute: 30}, function(){
+
+  function run (repo, callback) {
+    console.log('running');
+    var myAdj = _.shuffle(adjectives)[0];
+    github.getRepos({ q: myAdj }, function (error, res, body) {
+      if (error) {
+        console.log(error);
       }
+      var results = JSON.parse(body); 
+
+      return github.findGhPages(results.items, function(goods) {
+        if (goods.length) {
+          var theRepo = _.shuffle(goods)[0];
+          return github.getGhImg(theRepo, function(repo) {
+            twitter.tweet(repo, function(){
+              console.log('finished');
+            })
+          });
+        } else {
+          console.log('no result');
+        }
+      });
     });
-  });
+  }
+
+  var timesToRun = Math.floor(Math.random() * 10) + 1;
+
+  for (var i = timesToRun - 1; i >= 0; i--) {
+    run();
+  }
+
 });
