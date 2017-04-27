50643ad446273fcb7b5801416489e4f85a2a884a
Stuart Romanek <stuart@punkave.com>
2017-04-27 07:39:31 -0400

    leave scheduling to heroku

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var async = require('async');
 var request = require('request');
 var adjectives = require('adjectives');
 var _ = require('lodash');
-var schedule = require('node-schedule');
-var http = require('http');
 
 console.log('STARTING UP');
 
@@ -16,43 +14,34 @@ if (!process.env.twConsumerKey) {
 var github = require('./lib/github');
 var twitter = require('./lib/twitter');
 
-var j = schedule.scheduleJob({hour: 14, minute: 30}, function(){
+function run (repo, callback) {
+  console.log('running');
+  var myAdj = _.shuffle(adjectives)[0];
+  github.getRepos({ q: myAdj }, function (error, res, body) {
+    if (error) {
+      console.log(error);
+    }
+    var results = JSON.parse(body); 
 
-  function run (repo, callback) {
-    console.log('running');
-    var myAdj = _.shuffle(adjectives)[0];
-    github.getRepos({ q: myAdj }, function (error, res, body) {
-      if (error) {
-        console.log(error);
+    return github.findGhPages(results.items, function(goods) {
+      if (goods.length) {
+        var theRepo = _.shuffle(goods)[0];
+        return github.getGhImg(theRepo, function(repo) {
+          twitter.tweet(repo, function(){
+            console.log('finished');
+          })
+        });
+      } else {
+        console.log('no result');
       }
-      var results = JSON.parse(body); 
-
-      return github.findGhPages(results.items, function(goods) {
-        if (goods.length) {
-          var theRepo = _.shuffle(goods)[0];
-          return github.getGhImg(theRepo, function(repo) {
-            twitter.tweet(repo, function(){
-              console.log('finished');
-            })
-          });
-        } else {
-          console.log('no result');
-        }
-      });
     });
-  }
+  });
+}
 
-  var timesToRun = Math.floor(Math.random() * 10) + 1;
+var timesToRun = Math.floor(Math.random() * 10) + 1;
 
-  for (var i = timesToRun - 1; i >= 0; i--) {
-    run();
-  }
-
-});
-
-// try to make heroku happy
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-}).listen(process.env.PORT || 3000);
+for (var i = timesToRun - 1; i >= 0; i--) {
+  run();
+}
 
 console.log('this is the end');
